3c02cebfafee0c81681efff8343b27ebb6ec80a2
Matt Ho <matt.ho@gmail.com>
2021-07-13 16:12:24 -0500

    decreasing verbosity of action

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,11 @@
 set -eu
 
 # populate stack cache
-(cd /root && tar cf - .stack | (cd /github/home && tar xvf -))
+echo "copying stack cache to /github/home/.stack"
+(cd /root && tar cf - .stack | (cd /github/home && tar xf -))
+
+echo "changing ownership of /github/workspace"
 chown -R "$(id -un):$(id -gn)" /github/workspace
 
+echo "stack test"
 stack test
